8d47f0c83aed64135877c93c03e2625a88169512
Ashley Pringle <ash.pringle@gmail.com>
2015-04-16 13:17:27 -0300

    Added template, math source

--- assemble.bat
+++ assemble.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM assembles first.asm, using the included files in the "includes" directory
-nasm -i .\includes\ -f win32 first.asm
+nasm -i .\includes\ -f win32 first.asm -l first.lst
