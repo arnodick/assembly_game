756bfb88b99f401d2fcf0f271eaa0106fee4f54e
Ashley Pringle <ash.pringle@gmail.com>
2015-04-28 20:58:25 -0300

    Silly prog to take in 2 #s and convert from ASCII char to hex data in DX

--- assemble.bat
+++ assemble.bat
@@ -2,4 +2,5 @@
 
 REM assembles first.asm, using the included files in the "includes" directory
 REM nasm -i .\includes\ -f win32 first.asm -l first.lst
-nasm -f obj caverns.asm
+REM nasm -f obj caverns.asm MIGHT NEED THIS FOR LINKING DIFFERENT OBJ FILES IN PETEY N'S BOOK
+nasm -o caverns.com caverns.asm
--- caverns.asm
+++ caverns.asm
@@ -16,13 +16,30 @@ segment .bss
 
 segment .text
 
-	mov		ah, 2h				; output string with int 21
-	mov		dl, 2ah				; 2Ah = *
-	int		21h					; print char at AH
-	int		20h					; end code
+	mov 	ah, 01h				; set int to take a key input
+	int 	21h					; get ASCII key input, put it in AL
+	mov		dl, al				; move read ASCII value to DL
+	sub 	dl, 30h				; subtract 30 from char (convert digits to 0 - 9)
+	cmp		dl, 09h				; is it less than 9? (between 0 and 9)
+	jle		digit1				; if it is, jump to print first digit's 4 bit code
+	sub		dl, 07h				; if greater, subtract 7 from ASCII value (puts it in letter range A - F)
+digit1:
+	mov		cl, 04h				; prepare CX with 4 loops
+	shl 	dl, cl				; shift the ASCII value left 4 bits (multiply it by 16, bc it's the higher digit)
+	int 	21h					; read next ASCII char from keyboard
+	sub		al, 30h				; convert to number again
+	cmp		al, 9h				; is it 0 - 9 again?
+	jle		digit2				; if yes, print digit2
+	sub		al, 7h				; if not, convert to char
+digit2:
+	add		dl, al				; add first digit to oher, high digit
+	int		20h					; exit
+
+;display an asterix
+;	mov		ah, 2h				; output string with int 21
+;	mov		dl, 2ah				; 2Ah = *
+;	int		21h					; print char at AH
+;	int		20h					; end code
 
 ; code is put in the text segment. Do not modify the code before
 ; or after this comment.
-
-
-
